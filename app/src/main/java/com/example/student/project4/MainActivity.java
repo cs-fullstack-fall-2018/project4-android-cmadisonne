10682abe12f75de9f4092918e9615fac70be276f
Lotus <cmadisonne@gmail.com>
2018-11-29 12:43:38 -0600

    InProgress

--- app/src/main/java/com/example/student/project4/MainActivity.java
+++ app/src/main/java/com/example/student/project4/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.student.project4;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         mRecyclerView = findViewById(R.id.recyclerview);
 // Create an adapter and supply the data to be displayed.
@@ -42,5 +51,40 @@ public class MainActivity extends AppCompatActivity {
         }
 
         mAdapter.notifyDataSetChanged();
+    }
+
+    public void openWebsite(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/madisonne-cooper-61451a145/?trk=pub-pbmap"));
+        startActivity(intent);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        switch (item.getItemId()){
+            case R.id.share:
+                displayToast("Share");
+                return true;
+            case R.id.location:
+                displayToast("Created In Memphis,TN");
+                return true;
+            case R.id.developer:
+//                openWebsite(View );
+                return true;
+            default:
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+
     }
 }
